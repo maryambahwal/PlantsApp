047f78c9f50f19b778877f7afe35c7f6d437bcee
Maryam Bahwal <mrim2m7md@gmail.com>
2024-10-18 22:00:08 +0300

    Initial Commit

---
+++ PlantsApp/PlantsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsAppApp.swift
+//  PlantsApp
+//
+//  Created by Maryam Bahwal on 15/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
